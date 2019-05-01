11fd7f46f2b54cb33c071b531c46108ded0ababd
Donavan Armstrong <donavan.mc@gmail.com>
2019-04-30 17:38:17 -0700

    afc

--- js/script.js
+++ js/script.js
@@ -14,7 +14,45 @@ function showDivs(arrow) {
         images[i].style.display = "none";
     }
     images[slides - 1].style.display = "block";
-
+    
 }
 
+// modal
+
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
 
